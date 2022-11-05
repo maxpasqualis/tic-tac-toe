9f61120944e5a3b02e393c0b2b110a1921ce9d73
Maxwell <maxpasqualis@gmail.com>
2022-11-04 21:37:40 -0700

    added reset board func

--- tictactoe/classes/board.py
+++ tictactoe/classes/board.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 LENGTH = 3
 WIDTH = 3
@@ -6,3 +6,7 @@ WIDTH = 3
 class Board:
     def __init__(self):
         pass
+
+    def reset_board():
+        board = np.zeros((LENGTH,WIDTH), dtype=int)
+        return board
