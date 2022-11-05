859a5ededbf32db5e66d3f0ec00dac30b699ddf9
Maxwell <maxpasqualis@gmail.com>
2022-11-05 00:00:31 -0700

    planning structure

--- tictactoe/classes/board.py
+++ tictactoe/classes/board.py
@@ -5,8 +5,14 @@ WIDTH = 3
 
 class Board:
     def __init__(self):
+        # creates the working board instance
+        self.board = np.zeros((LENGTH,WIDTH), dtype=int)
+
+    def check_move_validity(self, x, y):
+        # helper function - checks if the specified move can be made
         pass
 
-    def reset_board():
-        board = np.zeros((LENGTH,WIDTH), dtype=int)
-        return board
+    def make_move(self, x, y, player):
+        # takes in xy coords of desired marker position and a Player instance
+        # if valid move, outputs new board with player's marker in specified position
+        pass
